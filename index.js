24c5dc687e26454e9d5f3201e058ebd305e20fc1
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-04-15 15:51:45 +0100

    Refactor server setup and update index.js

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ res.send('Hello, World! This is the root path.');
 
 
 app.get('/test', (req, res) => {
-res.send('This is a test route 2.');
+res.send('This is a test route.');
 });
 
 
 const PORT = 3000;
 
-mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+app.listen(PORT, () => {
+console.log(`Server is running on port ${PORT}`);
+});
