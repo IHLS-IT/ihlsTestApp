2b2ae19305f7bfa64c825c0367b89940e4edb647
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-04-15 15:24:58 +0100

    Update server setup and add MongoDB connection

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ res.send('This is a test route 2.');
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
