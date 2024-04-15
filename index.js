deb5a1e129f60b5a99c565404402782c58f2acc8
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-04-15 15:14:50 +0100

    Add .env file and update server connection in index.js

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ res.send('Hello, World! This is the root path.');
 
 
 app.get('/test', (req, res) => {
-res.send('This is a test route.');
+res.send('This is a test route 2.');
 });
 
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
